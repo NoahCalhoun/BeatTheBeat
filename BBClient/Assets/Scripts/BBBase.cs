a6cd79c31762ba4336ae72bd32da4a72121fe638
NoahCalhoun <37259375+NoahCalhoun@users.noreply.github.com>
2018-10-17 03:11:06 +0900

    클라이언트 세팅

---
+++ BBClient/Assets/Scripts/BBBase.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class BBBase : MonoBehaviour
+{
+    private Transform mTm;
+    public Transform Tm { get { if (mTm == null) mTm = transform; return mTm; } }
+
+
+    private RectTransform mRt;
+    public RectTransform Rt { get { if (mRt == null) mRt = transform as RectTransform; return mRt; } }
+
+
+    private GameObject mGo;
+    public GameObject Go { get { if (mGo == null) mGo = gameObject; return mGo; } }
+}
---
+++ BBClient/Assets/Scripts/TestUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TestUI : MonoBehaviour, IPointerClickHandler
+{
+    public delegate void OnClick();
+
+    public OnClick OnClickEvent;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        OnClickEvent();
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        OnClickEvent = () => { GetComponent<RectTransform>().localPosition += new Vector3(5f, 0f); };
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.localPosition += (new Vector3(5f, 0f) * Time.deltaTime);
+    }
+}
